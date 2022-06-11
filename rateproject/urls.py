b446d72f9b94fef474c813082854d0538ddbf8d9
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-11 09:44:42 +0300

    feat: registration and creatsuperuser

--- rateproject/urls.py
+++ rateproject/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', include('rate.urls'))
+    path(r'', include('rate.urls')),
+    re_path(r'^accounts/', include('registration.backends.simple.urls')),
+    
 ]
