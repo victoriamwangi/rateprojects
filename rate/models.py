e192f1ed3c1684a109d13a04be67be8677040898
victoria mwangi <victoriamwangi021@gmail.com>
2022-06-11 10:52:11 +0300

    project model and image upload

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    proj_name= models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    proj_description = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'projects/')
--- rateproject/urls.py
+++ rateproject/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path(r'', include('rate.urls')),
+    re_path('admin/', admin.site.urls),
+    re_path(r'', include('rate.urls')),
     re_path(r'^accounts/', include('registration.backends.simple.urls')),
+    re_path(r'^logout/$', auth_views.logout_then_login), 
+
     
 ]
